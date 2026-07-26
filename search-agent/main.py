6fccca571076eb9c8263af52f49d87681a1cc44c
Jawahar <jawaharr1393@gmail.com>
2026-07-26 19:59:22 +0530

    using Langchain Native Tavilysearch with Linked Job search query for AI Engineer

--- search-agent/main.py
+++ search-agent/main.py
@@ -8,29 +8,31 @@ from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 
-tavily = TavilyClient()
-@tool
-def search(query: str) -> str:
-    """"Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The Search Result
-        """
-    print(f"searching for {query}")
-    #return "Berlin weather is rainy"
-    return tavily.search(query=query)
+# tavily = TavilyClient()
+# @tool
+# def search(query: str) -> str:
+#     """"Tool that searches over internet
+#     Args:
+#         query: The query to search for
+#     Returns:
+#         The Search Result
+#         """
+#     print(f"searching for {query}")
+#     #return "Berlin weather is rainy"
+#     return tavily.search(query=query)
 
 llm = ChatOllama(temperature=0, model="gpt-oss:20b")
-tools = [search]
+#tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from agentic-ai-learning!")
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Berlin?")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer in the Berlin Area on linkedIn and list their details")})
     pprint(result)
 
 
